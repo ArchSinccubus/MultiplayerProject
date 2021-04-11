7b0800ad1e98d4080aeff8d2fb5ec3090f2c0e08
ArchSinccubus <Edenben13@hotmail.com>
2021-04-11 12:55:21 +0300

    Init push

---
+++ Assets/CarController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float maxSpeed = 100f;
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 1000.0F;
+    public float translation, rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        translation = Input.GetAxis("Vertical") * speed;
+        rotation += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        translation *= Time.deltaTime;
+        Debug.Log(rotation);
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            Vector3 movement = Quaternion.Euler(0, rotation, 0) * Vector3.forward * translation;
+
+            rb.AddForce(movement, ForceMode.VelocityChange);
+        }
+
+        //Debug.Log(rb.velocity);
+        rb.MoveRotation(Quaternion.Euler(0, rotation, 0));
+    }
+}
